d8d5cada123fe2a9ee2647f6fec5f5bf06758e28
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-12-14 12:49:19 -0800

    Add reproducer for wrong completions

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,8 +1,13 @@
-use passthrough::passthrough;
+use std::vec::IntoIter;
+
+use passthrough::{passthrough, passthrough_wrapped};
 
 fn main() {
     let test1 = 123;
     passthrough!({
         let test2 = 123;
-    })
+    });
+
+    let my_iter: IntoIter<Result<i32, ()>> = vec![Ok(1), Ok(2), Ok(3)].into_iter();
+    let _ = my_iter.map(passthrough_wrapped!(|x: Result<i32, ()>| x.unwrap().abs()));
 }
--- passthrough/src/lib.rs
+++ passthrough/src/lib.rs
@@ -1,7 +1,28 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
 
 #[proc_macro]
 pub fn passthrough(input: TokenStream) -> TokenStream {
     input
+}
+
+#[proc_macro]
+pub fn passthrough_wrapped(input: TokenStream) -> TokenStream {
+    let input_parsed = syn::parse_macro_input!(input as syn::Expr);
+    let input_without_spans: syn::Expr = syn::parse_str(
+        &input_parsed.to_token_stream().to_string()
+    ).unwrap();
+    quote! {
+        (|| {
+            if false {
+                let _ = #input_without_spans;
+            }
+
+            #[allow(unreachable_code)]
+            {
+                #input_parsed
+            }
+        })()
+    }.into()
 }
